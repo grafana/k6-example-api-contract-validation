135309da7f7fa94941aefd29650d89688621c675
Pawel Suwala <pawel.suwala@gmail.com>
2021-05-07 14:50:57 +0200

    moved some files around

--- api_contracts/contracts.js
+++ api_contracts/contracts.js
@@ -1,4 +1,7 @@
-import { crocodileAPIContract } from './crocodile_service/objects/crocodile.js'
+// This file collects all contracts in one place 
+// This makes it easy to import in the k6 script
+
+import { crocodileAPIContract } from './crocodile_service/endpoints/crocodile.js'
 import { crocodileListAPIcontract } from './crocodile_service/endpoints/croc_list.js'
 import { registerAPIcontract, registerAPIResponseContract } from './registration_service/endpoints/register.js'
 import { tokenAuthRequestAPIcontract, tokenAuthResponseAPIcontract } from './authentication_service/endpoints/auth_token.js'
